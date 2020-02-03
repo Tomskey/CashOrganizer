ca77a864e41193a42cb027ef045e961b10126266
Thomskey <tomasz.sokolowski.t@gmail.com>
2020-02-03 19:41:34 +0100

    Initial commit

---
+++ src/main/java/com/CashOrganizer/CashOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.CashOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CashOrganizer/CashOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CashOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
